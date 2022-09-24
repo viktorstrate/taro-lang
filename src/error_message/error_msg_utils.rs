20964e5313d7f4b29ab9e1608e5ef4c20e8816c2
viktorstrate <viktorstrate@gmail.com>
2022-09-24 21:09:16 +0200

    improve performance of lev_dist

--- src/error_message/error_msg_utils.rs
+++ src/error_message/error_msg_utils.rs
@@ -1,28 +1,29 @@
 use std::cmp::min;
 
 pub fn lev_dist(src: &str, tar: &str) -> usize {
-    let m = src.len() + 1;
     let n = tar.len() + 1;
-    let mut d = vec![vec![0usize; n]; m];
 
-    for i in 1..m {
-        d[i][0] = i;
-    }
+    let mut v0 = vec![0usize; n];
+    let mut v1 = vec![0usize; n];
 
-    for j in 1..n {
-        d[0][j] = j;
+    for i in 1..n {
+        v0[i] = i;
     }
 
     for (i, s) in src.chars().enumerate() {
+        v1[0] = i + 1;
+
         for (j, t) in tar.chars().enumerate() {
-            let mut min_cost = d[i][j + 1] + 1;
-            min_cost = min(min_cost, d[i + 1][j] + 1);
-            min_cost = min(min_cost, d[i][j] + if s == t { 0 } else { 1 });
-            d[i + 1][j + 1] = min_cost;
+            let mut min_cost = v0[j + 1] + 1;
+            min_cost = min(min_cost, v1[j] + 1);
+            min_cost = min(min_cost, v0[j] + if s == t { 0 } else { 1 });
+            v1[j + 1] = min_cost;
         }
+
+        std::mem::swap(&mut v0, &mut v1);
     }
 
-    d[m - 1][n - 1]
+    v0[n - 1]
 }
 
 #[cfg(test)]
